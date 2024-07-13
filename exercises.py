f9ce7aebc26ee0392b895db563076831f2e8dfe7
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-13 16:04:55 -0700

    V07132024 4:04PM

--- exercises.py
+++ exercises.py
@@ -10,5 +10,12 @@ def unique_characters(s:str) -> bool:
 print(unique_characters("Futbol"))
 print(unique_characters("Soccer"))
 
-
-        
+#1.2 Is permutation
+def is_permutation(str1:str,str2:str) ->bool:
+    if len(str1)!=len(str2):
+        return False
+    if sorted(str1)==sorted(str2):
+        return True
+    
+print(is_permutation("dog","god"))
+print(is_permutation("cow","wocc"))
