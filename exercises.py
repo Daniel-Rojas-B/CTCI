1d886f010100f351460a59960d6225d3094591bf
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-13 17:57:58 -0700

    V07132024 5:57PM

--- exercises.py
+++ exercises.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 #1.1 Is unique
 def unique_characters(s:str) -> bool:
     length = len(s)
@@ -29,5 +31,23 @@ print(replace_with_20("tomorrow soccer"))
 
 #1.3 Option 2: Replace with %20 pending 
 # def replace_with_20v2(s:str)->str:
+
+#1.4 is_palindrome_permutation
+
+def is_palindrome_permutation(s:str)->bool:
+    #String without spaces
+    basic_string = ''.join(s.split()).lower()
+    #Count the frequency of each character
+    count_characters=Counter(basic_string)
+
+    #It can only be one character with count 1 or 0
+    less_or_equals_one = sum(1 for count in count_characters.values() if count %2!=0)
+
+    return less_or_equals_one<=1
+    
+print(is_palindrome_permutation("tact coa"))
+print(is_palindrome_permutation("am layi la m"))
+
+
 
 
